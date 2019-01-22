f54c0951db367afba93b63376e6a0e1b93dc080e
zsx <git@zsxsoft.com>
2019-01-22 10:38:49 +0800

    Make PHPCS Happy

--- validator/Wrappers/ZBPInstaller.php
+++ validator/Wrappers/ZBPInstaller.php
@@ -57,59 +57,59 @@ class ZBPInstaller
 
     protected function gitPull()
     {
-        `git pull ${$this->gitPath
-    }`;
-}
+        $gitPath = $this->gitPath;
+        `git pull $gitPath`;
+    }
 
-protected function gitVersion()
-{
-    chdir($this->gitPath);
-    $ret = `git log --pretty="%h" -n1 HEAD`;
-    chdir('../../');
-    return $ret;
-}
+    protected function gitVersion()
+    {
+        chdir($this->gitPath);
+        $ret = `git log --pretty="%h" -n1 HEAD`;
+        chdir('../../');
+        return $ret;
+    }
 
-protected function extract()
-{
-    $webPath = $this->webPath;
-    PathHelper::rrmdir($webPath);
-    TempHelper::createTemp();
-    @mkdir($webPath);
+    protected function extract()
+    {
+        $webPath = $this->webPath;
+        PathHelper::rrmdir($webPath);
+        TempHelper::createTemp();
+        @mkdir($webPath);
 
-    if ($this->isUsingGit()) {
-        PathHelper::rcopy($this->gitPath, $webPath);
-    } else {
-        $xml = simplexml_load_file($this->xmlPath, 'SimpleXMLElement');
-        $old = umask(0);
-        foreach ($xml->file as $f) {
-            $filename = $webPath . DIRECTORY_SEPARATOR . str_replace('\\', '/', $f->attributes());
-            $dirname = dirname($filename);
-            @mkdir($dirname, 0755, true);
-            file_put_contents($filename, base64_decode($f));
+        if ($this->isUsingGit()) {
+            PathHelper::rcopy($this->gitPath, $webPath);
+        } else {
+            $xml = simplexml_load_file($this->xmlPath, 'SimpleXMLElement');
+            $old = umask(0);
+            foreach ($xml->file as $f) {
+                $filename = $webPath . DIRECTORY_SEPARATOR . str_replace('\\', '/', $f->attributes());
+                $dirname = dirname($filename);
+                @mkdir($dirname, 0755, true);
+                file_put_contents($filename, base64_decode($f));
+            }
         }
     }
-}
 
-protected function createEmptyEnvironment()
-{
-    if (!$this->isUsingGit()) {
-        if (!is_file($this->xmlPath)) {
-            Logger::info('Downloading latest Z-BlogPHP...');
-            $this->download();
+    protected function createEmptyEnvironment()
+    {
+        if (!$this->isUsingGit()) {
+            if (!is_file($this->xmlPath)) {
+                Logger::info('Downloading latest Z-BlogPHP...');
+                $this->download();
+            }
         }
-    }
-    Logger::info('Cleaning Environment');
-    if ($this->isUsingGit()) {
-        $version = ZBPInstaller::gitVersion();
-        Logger::info("Using Git {$version}Z-BlogPHP");
-    } else {
-        Logger::info('Using latest stable Z-BlogPHP');
-    }
-    $this->extract();
+        Logger::info('Cleaning Environment');
+        if ($this->isUsingGit()) {
+            $version = ZBPInstaller::gitVersion();
+            Logger::info("Using Git {$version}Z-BlogPHP");
+        } else {
+            Logger::info('Using latest stable Z-BlogPHP');
+        }
+        $this->extract();
 
-    PathHelper::rcopy(PathHelper::getAbsoluteFilename(ROOT_PATH . '/resources/zb_users'), $this->webPath . '/zb_users');
+        PathHelper::rcopy(PathHelper::getAbsoluteFilename(ROOT_PATH . '/resources/zb_users'), $this->webPath . '/zb_users');
 
-    Logger::info('Cleaned successfully');
-}
+        Logger::info('Cleaned successfully');
+    }
 
 }
